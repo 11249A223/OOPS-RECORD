fdbdbaea86cfe521213425bcc614ad2c981196d5
11249A223 <11249a223@kanchiuniv.ac.in>
2025-11-07 23:35:30 -0800

    Introduction to Graphics — Console Bar Chart.cpp

---
+++ Introduction to Graphics — Console Bar Chart.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    vector<int> marks(5);
+
+    cout << "Enter marks for 5 subjects (0-100):\n";
+    for (int i = 0; i < 5; ++i) {
+        cin >> marks[i];
+        if (marks[i] < 0) marks[i] = 0;       // clamp to 0
+        if (marks[i] > 100) marks[i] = 100;   // clamp to 100
+    }
+
+    cout << "\nText Bar Chart (each * = 2 marks)\n\n";
+    for (int i = 0; i < 5; ++i) {
+        cout << "Sub" << (i+1) << " [" << setw(3) << marks[i] << "]: ";
+
+        int bars = marks[i] / 2;  // each '*' represents 2 marks
+        for (int j = 0; j < bars; ++j)
+            cout << '*';
+
+        cout << '\n';
+    }
+
+    return 0;
+}
